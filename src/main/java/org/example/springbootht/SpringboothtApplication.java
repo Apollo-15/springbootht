3c2c95fbaf135f33709d79ddd1934dd28fa2e579
Appolo-15 <rayl1x.gta@gmail.com>
2025-06-08 17:47:51 +0200

    Initial commit

---
+++ src/main/java/org/example/springbootht/SpringboothtApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringboothtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootht/SpringboothtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboothtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
